33c25a4e743571c7210bafad8d120bbfb3cbbd63
Mariano Passerini <renacer258@gmail.com>
2023-08-23 23:12:22 -0300

    map exercise

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,31 +7,38 @@ const Header = ({course}) => {
 const Content = (props) => {
   return(
     <div>
-      {props.part1} exercise = {props.exercises1} {"\n"}
-      {props.part2} exercise = {props.exercises2}
-      {props.part3} exercise = {props.exercises3}
+      <p>{props.part} exercise = {props.exercises} </p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <Header course = {course}/>
-      <Content  part1 = {part1} exercises1 = {exercises1}
-                part2 = {part2} exercises2 = {exercises2}
-                part3 = {part3} exercises3 = {exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Header course={course.name} />
+        {course.parts.map((part, index) => (
+          <Content key={index} part={part.name} exercises={part.exercises} />
+        ))}
+      </div>
+    )
+  }
 
 export default App
